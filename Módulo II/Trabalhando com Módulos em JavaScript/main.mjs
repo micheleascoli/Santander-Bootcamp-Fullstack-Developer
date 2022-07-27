5144c3a88478aae367ecbc8298247b4b89ffa634
micheleascoli <micheleascoli17@gmail.com>
2022-07-27 13:47:53 -0500

    finalizando os cursos Manipulando a DOM com JavaScript e Módulos em JS

---
+++ Módulo II/Trabalhando com Módulos em JavaScript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Michele", 25));
+console.log(mostraCidade("Michele", "Rio Branco"));
+console.log(mostraHobby("Michele", "assistir séries"));
